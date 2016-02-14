d5b3af54742cf6fa248b3a6b60d948efc99146e3
David LePage <dlepage70@gmail.com>
2016-02-14 14:49:44 -0600

    Changed action to run in mfeagent.rb

--- cookbooks/mcafee/recipes/mfeagent.rb
+++ cookbooks/mcafee/recipes/mfeagent.rb
@@ -23,33 +23,33 @@ mcafee = data_bag_item("mcafee-databag", "data")
 remote_file "/tmp/agentPackages.zip" do
   source "#{mcafee['http']['http_host']}/agentPackages.zip"
   action :create_if_missing
-  notifies :run, 'execute[unpack_MA]', :immediately
-  notifies :run, 'execute[provide_exe_permission]', :immediately
-  notifies :run, 'execute[install_mfe_agent]', :immediately
-  notifies :run, 'execute[contact_epo]', :immediately
+##  notifies :run, 'execute[unpack_MA]', :immediately
+#  notifies :run, 'execute[provide_exe_permission]', :immediately
+#  notifies :run, 'execute[install_mfe_agent]', :immediately
+#  notifies :run, 'execute[contact_epo]', :immediately
 end
 
 #Unpack mcafee agent
 execute 'unpack_MA' do
   command "sudo unzip -o /tmp/agentPackages.zip"
-  action :nothing
+  action :run
 end
 
 #Add execute to installer
 execute 'provide_exe_permission' do
   command "sudo chmod a+x install.sh"
-  action :nothing
+  action :run
 end
 
 #Install McAfee Agent
 execute 'install_mfe_agent' do
   command "sudo ./install.sh -i"
-  action :nothing
+  action :run
 end
 
 execute 'contact_epo' do
   command "sudo /opt/McAfee/cma/bin/cmdagent -P"
-  action :nothing
+  action :run
 end
 
 #---------------------------End of Recipe-----------------------------------
