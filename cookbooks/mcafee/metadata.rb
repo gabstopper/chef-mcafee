0cc5ce50ce6142aa237643144a39de213436adfd
David LePage <dlepage70@gmail.com>
2016-03-07 14:13:31 -0600

    remove unneeded

--- cookbooks/mcafee/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'mcafee'
-maintainer       'Intel Security'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures McAfee Public Cloud'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.5'
-depends		 'windows'
--- cookbooks/mcafee/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: mcafee
-# Recipe:: default
-#
-# Copyright 2016, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
-Chef::Log.info("Running default recipe")
