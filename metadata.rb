06d2a47d8bbfdf9bf3dd8711e91102b0a0d1c9ee
David LePage <dlepage70@gmail.com>
2016-03-07 19:43:54 -0600

    modified metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'mcafee'
-maintainer       'Intel Security'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'David LePage'
+maintainer_email 'dwlepage70@gmail.com'
 license          'All rights reserved'
-description      'Installs/Configures McAfee Public Cloud'
+description      'Installs/Configures McAfee Public Cloud endpoint suite'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.5'
 depends		 'windows'
