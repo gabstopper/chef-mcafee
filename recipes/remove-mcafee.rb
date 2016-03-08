f11e881819c7c401f5c5ec0a492eb77c73dad349
David LePage <dlepage70@gmail.com>
2016-03-07 19:44:10 -0600

    fixed comments

--- recipes/remove-mcafee.rb
+++ recipes/remove-mcafee.rb
@@ -1,12 +1,12 @@
 #
 # Cookbook Name:: mcafee
-# Recipe:: dpclinux
+# Recipe:: remove-mcafee
 #
 # Copyright 2016, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
 #
-# Remove mcafee agent last
+# Remove mcafee agent last, can use delayed execution in recipe
 
 mcafee 'vse' do
   action :remove
