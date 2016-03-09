11430ab268af6e74afa1ce57f83257a24731c91c
David LePage <dlepage70@gmail.com>
2016-03-09 17:58:31 -0600

    remove all mcafee products recipe

---
+++ recipes/mcafee-remove.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: mcafee
+# Recipe:: remove-mcafee
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+# Remove mcafee agent last, can use delayed execution in recipe
+
+mcafee 'vse' do
+  action :remove
+end
+
+mcafee 'dpc' do
+  action :remove
+end
+
+case node['platform_family']
+when 'rhel', 'debian'
+  mcafee 'agent' do
+    workdir "/tmp"
+    action :remove
+  end
+when 'windows'
+  mcafee 'agent' do
+    workdir "/tmp"
+    product_info ({
+      :package => 'FramePkg.exe',
+      :installer => 'FramePkg.exe',
+      :install_key => %w(McAfee\ Agent)}
+    )
+    #url s3.amazon.com
+    #cookbook_file blah
+    #uncpath \\share
+    action :remove
+  end
+end
+
+#-------------------------End of Recipe-----------------------------------
+
+
