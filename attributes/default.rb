4454ab3e72972988b6c268947232b9c1f998f793
David LePage <dlepage70@gmail.com>
2016-03-12 19:24:36 -0600

    use normal array instead of whitespace

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,32 +8,38 @@ case node['platform_family']
     default['mcafee']['vse'] = {
       'package' => 'McAfeeVSEForLinux-2.0.2.29099-release.tar.gz',
       'installer' => 'McAfeeVSEForLinux-2.0.2.29099-installer',
-      'install_key' => %w(mcafeevseforlinux)
+      'install_key' => ['McAfeeVSEForLinux', 'ISecGRt']
+      #'install_key' => %w(McAfeeVSEForLinux ISecGRt)
     }
     default['mcafee']['dpc'] = {
       'package' => 'MDPC1.0_Deployment_Linux.zip',
       'installer' => 'install.sh',
-      'install_key' => %w(dpc)
+      'install_key' => ['dpc']
+      #'install_key' => %w(dpc)
     }
     default['mcafee']['agent'] = { 
       'package' => 'agentPackages.zip',
       'installer' => 'install.sh',
-      'install_key' => %w(mfecma mfert)
+      'install_key' => ['MFEcma', 'MFErt']
+      #'install_key' => %w(MFEcma MFErt)
     }
   when 'windows'
     default['mcafee']['agent'] = {
       'package' => 'FramePkg.exe',
       'installer' => 'FramePkg.exe',
-      'install_key' => %w(McAfee\ Agent)
+      'install_key' => ['McAfee Agent']
+      #'install_key' => %w(McAfee\ Agent)
     }
     default['mcafee']['vse'] = {
       'package' => 'VSE880LMLRP7.Zip',
       'installer' => 'SetupVSE.exe',
-      'install_key' => %w(McAfee\ VirusScan\ Enterprise)
+      'install_key' => ['McAfee VirusScan Enterprise']
+      #'install_key' => %w(McAfee\ VirusScan\ Enterprise)
     }
     default['mcafee']['dpc'] = {
       'package' => 'MDPC1.0_Deployment_Windows.zip',
       'installer' => 'MDPCAgent.msi',
-      'install_key' => %w(McAfee\ Data\ Protection\ Agent)
+      'install_key' => ['McAfee Data Protection Agent']
+      #'install_key' => %w(McAfee\ Data\ Protection\ Agent)
     }
   end
