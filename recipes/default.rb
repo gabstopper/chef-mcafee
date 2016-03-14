435a9bc7674e4fee1da2158f32efc19fc69bafaa
David LePage <dlepage70@gmail.com>
2016-03-14 10:21:20 -0500

    modified ruby block, unsure if this will remain

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,13 +9,10 @@
 
 #workaround requiretty when sudo is run on rhel platforms
 
-sudoers = "/etc/sudoers"
-requiretty = /^(Defaults\s+requiretty)/i
-
 ruby_block "tmp disable requiretty" do
   block do
-    sed = Chef::Util::FileEdit.new(sudoers)
-    sed.search_file_replace(requiretty, '#\1')
+    sed = Chef::Util::FileEdit.new("/etc/sudoers")
+    sed.search_file_replace(/^(Defaults\s+requiretty)/i, '#\1')
     sed.write_file
   end
   only_if { ::File.readlines(sudoers).grep(requiretty).any? }
