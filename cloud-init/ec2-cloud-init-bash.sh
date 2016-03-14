7f662e2d3eed945883c54c87be3f70d7dddde7bb
David LePage <dlepage70@gmail.com>
2016-03-14 15:41:27 -0500

    add cloud-init userdata

---
+++ cloud-init/ec2-cloud-init-bash.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#tested on AWS ubuntu, redhat and amazon linux
+#install chef omnibus client
+true && curl -L https://www.opscode.com/chef/install.sh | bash
+mkdir /etc/chef
+
+#make sure it's an amazon instance os
+if [[ $(dmidecode | grep -i amazon) ]]
+then
+  os=$(uname -a)
+  echo $os
+  if grep -qv "amzn" <<< "$os"	#anything but amazon linux
+  then
+    if grep -iqe '^Defaults[[:space:]]\+requiretty' /etc/sudoers	#make sure we'll have ability to sudo
+	then
+	  echo "Adding /etc/sudoers.d file to allow sudo without tty"
+	  echo -e 'Defaults:ec2-user !requiretty\nDefaults:root !requiretty' > /etc/sudoers.d/99-cloud-init
+	fi  
+  fi
+  #aws tools
+  curl -O https://bootstrap.pypa.io/get-pip.py
+  python get-pip.py
+  pip install awscli
+  #instance region for running aws s3
+  region=`curl -s 169.254.169.254/latest/meta-data/placement/availability-zone`
+  region=${region::-1}
+  export AWS_DEFAULT_REGION=$region
+fi
+
+
+#copy chef solo and node files
+aws s3 cp s3://chef-lepages/linux/node.json /etc/chef
+aws s3 cp s3://chef-lepages/linux/solo.rb /etc/chef
+
+#if run from cloud-init userdata, root home directory is not yet set to /root when this is run
+export HOME=/root
+
+#execute solo chef run
+chef-solo -j /etc/chef/node.json -c /etc/chef/solo.rb --recipe-url https://s3-us-west-2.amazonaws.com/chef-lepages/linux/mcafee-linux-0.1.8.tar.gz
