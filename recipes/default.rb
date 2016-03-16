ac3a103a256f784de3c852b45dea4947b86eaec3
David LePage <dlepage70@gmail.com>
2016-03-16 08:00:01 -0500

    removed sudo requirement

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,11 +9,3 @@
 
 #workaround requiretty when sudo is run on rhel platforms
 
-ruby_block "tmp disable requiretty" do
-  block do
-    sed = Chef::Util::FileEdit.new("/etc/sudoers")
-    sed.search_file_replace(/^(Defaults\s+requiretty)/i, '#\1')
-    sed.write_file
-  end
-  only_if { ::File.readlines(sudoers).grep(requiretty).any? }
-end if platform_family?('rhel')
