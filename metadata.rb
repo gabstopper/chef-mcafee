b54f5dcca0b96bd6a985d2597f2d17b0709ca291
David LePage <dwlepage70@gmail.com>
2016-03-18 10:11:14 -0500

    up version

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'dwlepage70@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures McAfee Public Cloud endpoint suite'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.9'
+version          '0.2.0'
 depends		 'windows'
